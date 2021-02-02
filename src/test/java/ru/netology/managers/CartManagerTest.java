5df54171fda96155736eb34a5cf59aac78703a82
balrom1981 <balrom@bk.ru>
2021-02-02 17:39:01 +0300

    Initial commit

---
+++ src/test/java/ru/netology/managers/CartManagerTest.java
@@ -0,0 +1,29 @@
+package ru.netology.managers;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.PurchaseItem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartManagerTest {
+
+    @Test
+    void shouldRemoveIfExists() {
+        CartManager manager = new CartManager();
+        int idToRemove = 1;
+        PurchaseItem first = new PurchaseItem(1, 1, "first", 1,1);
+        PurchaseItem second = new PurchaseItem(2, 2, "second", 1,1);
+        PurchaseItem third = new PurchaseItem(3,3, "third", 1,1);
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+        manager.removeById(idToRemove);
+
+        PurchaseItem[] actual = manager.getAll();
+        PurchaseItem[] expected = new PurchaseItem[] {third, second};
+
+        assertArrayEquals(actual, expected);
+    }
+
+}
